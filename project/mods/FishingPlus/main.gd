744286bab404e2d7fbcd0d60caea633e722d2cfb
ellie <81702700+yeeterlol@users.noreply.github.com>
2024-11-18 19:10:10 -0800

    1.5.6
    add manifestation and r2modman support

--- project/mods/FishingPlus/main.gd
+++ project/mods/FishingPlus/main.gd
@@ -13,14 +13,43 @@ onready var the_button = preload("res://mods/FishingPlus/ui/button/fishingplus.t
 
 
 
-var gdweave_directory := OS.get_executable_path() + "/../GDWeave/"
-var configs_directory := gdweave_directory + "configs/"
+var gdweave_directory := _get_directory()
+var configs_directory := gdweave_directory.plus_file("configs")
 var _dir := Directory.new()
 var _file := File.new()
 
 func _ready():
 	_get_settings()
 	pass # Replace with function body.
+	
+		
+func _get_directory() -> String:
+	var game_directory := OS.get_executable_path().get_base_dir()
+	var default_directory := game_directory.plus_file("GDWeave")
+	var folder_override: String
+	var final_directory: String
+	
+	for argument in OS.get_cmdline_args():
+		if argument.begins_with("--gdweave-folder-override="):
+			folder_override = argument.trim_prefix("--gdweave-folder-override=").replace("\\", "/")
+	
+	if folder_override:
+		var relative_path := game_directory.plus_file(folder_override)
+		var is_relative := not ":" in relative_path and _file.file_exists(relative_path)
+		
+		final_directory = relative_path if is_relative else folder_override
+	else:
+		# if they are using env variables for some reason
+		if OS.has_environment("GDWEAVE_FOLDER_OVERRIDE"):
+			folder_override = OS.get_environment("GDWEAVE_FOLDER_OVERRIDE").replace("\\", "/")
+			var relative_path := game_directory.plus_file(folder_override)
+			var is_relative := not ":" in relative_path and _file.file_exists(relative_path)
+		
+			final_directory = relative_path if is_relative else folder_override
+		else:
+			final_directory = default_directory
+	
+	return final_directory
 
 func _get_settings():
 	# quick and dirty tacklebox solution
@@ -28,33 +57,21 @@ func _get_settings():
 	if _dir.open(configs_directory) != OK:
 		push_error("Could not open config directory")
 		return
-	
-	_dir.list_dir_begin(true, true)
-	var file_name := _dir.get_next()
-	while file_name != "":
-		
-		var config_path := configs_directory + file_name
-		var mod_id := file_name.replace(".json", "")
-		if mod_id == "FishingPlus":
-			_file.open(config_path, File.READ)
-			var config_data := JSON.parse(_file.get_as_text())
-			if config_data.error == OK and config_data.result is Dictionary:
-				# slop ass code
 
-				percent = config_data.result["CurrentFishChance"]
-				possible = config_data.result["LootTableView"]
-				patient = config_data.result["PatientLure"]
-				autocollect = config_data.result["AutoCollectBuddies"]
-				autobait = config_data.result["AutoSelectBait"]
-				fishnotify = config_data.result["NeedCertainFish"]
-				current_fish = config_data.result["FishIDs"]
-			_file.close()
-			break
-			
-		
-		file_name = _dir.get_next()
+	var config_path := configs_directory.plus_file("FishingPlus.json")
+	_file.open(config_path, File.READ)
+	var config_data := JSON.parse(_file.get_as_text())
+	if config_data.error == OK and config_data.result is Dictionary:
+		percent = config_data.result["CurrentFishChance"]
+		possible = config_data.result["LootTableView"]
+		patient = config_data.result["PatientLure"]
+		autocollect = config_data.result["AutoCollectBuddies"]
+		autobait = config_data.result["AutoSelectBait"]
+		fishnotify = config_data.result["NeedCertainFish"]
+		current_fish = config_data.result["FishIDs"]
+		_file.close()
 func _update_config():
-	var file = configs_directory + "FishingPlus.json"
+	var file = configs_directory.plus_file("FishingPlus.json")
 	var config_file_err := _file.open(file, File.WRITE)
 	if config_file_err != OK:
 		return config_file_err
